73ea7d2fd7949dc5e779a1282c28fbd87a7e5598
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2022-06-21 22:27:38 +0200

    Change from GPL2 to BSD3
    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- statquest/statquest.py
+++ statquest/statquest.py
@@ -6,14 +6,45 @@ The main module of StatQuest.
 File:
     project: StatQuest
     name: statquest.py
-    version: 0.4.0.0
+    version: 0.4.0.1
     date: 19.06.2022
 
 Authors:
     Sławomir Marczyński, slawek@zut.edu.pl
 
-Copyright (c) 2022 Sławomir Marczyński, slawek@zut.edu.pl.
+Copyright (c) 2022 Sławomir Marczyński, slawek@zut.edu.pl
 """
+
+#   BSD 3-Clause License
+#
+#   Copyright (c) 2022 Sławomir Marczyński
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are met:
+#
+#   1.  Redistributions of source code must retain the above copyright notice,
+#        this list of conditions and the following disclaimer.
+#
+#   2.  Redistributions in binary form must reproduce the above copyright
+#        notice, this list of conditions and the following disclaimer in the
+#        documentation and/or other materials provided with the distribution.
+#
+#   3.  Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+#   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+#   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+#   OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from statquest_input import input_observables
 from statquest_output import *
